332e32baac0d7fc5c2d533163fc72081352daeec
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-24 00:19:03 +0200

    Built from 833b87a on branch master

--- resources/terminal.js
+++ resources/terminal.js
@@ -1,20 +1,26 @@
 Reveal.addEventListener('fragmentshown', function (e) {
-    var command = $(e.fragment).attr('run-command');
-    if (command) {
+    var runCommand = $(e.fragment).attr('run-command');
+    if (runCommand !== undefined) {
         var terminal = $(e.fragment).parent('section').find('.terminal:not(.executed)');
         if (terminal.length) {
+            var command = terminal.find('.command .text').text().trim();
+            var commandTime = terminal.attr('time') || 2000;
+            var commandDelay = terminal.attr('delay') || 1500;
+            var typeSpeed = +terminal.attr('speed') || 80;
             terminal.find('.command').typed({
-                strings: ["npm init -y"],
-                typeSpeed: 80,
+                strings: [command],
+                typeSpeed: typeSpeed,
                 callback: function () {
                     setTimeout(function () {
-                        terminal.addClass('executing');
+                        if (commandTime > 0) {
+                            terminal.addClass('executing');
+                        }
                         setTimeout(function () {
                             terminal.addClass('executed');
                             terminal.removeClass('executing');
-                            terminal.find('.result').html($(e.fragment).html().trim());
-                        }, 2000);
-                    }, 1500);
+                            // terminal.find('.result').html($(e.fragment).html().trim());
+                        }, commandTime);
+                    }, commandDelay);
                 }
             });
         }
